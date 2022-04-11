25dab0be7f58f2aacfa8a362c47a857f832b6196
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-11 22:23:38 +0530

    [Manoj]Adding_LastName

---
+++ UserRegistration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class UserRegistration
+    {
+        public static bool NameValidation(String firstName)
+        {
+            String pattern = "^[A-Z]{1}[a-z]{2,}$";  //Define Pattern
+            Regex regex = new Regex(pattern);      //create object of the Regex class (its Regesx predefine class)
+            return regex.IsMatch(firstName);         //match in a specified input string
+        }
+        static void Main(string[] args)    //Main Method
+        {
+            Console.WriteLine("Welcome to User Registration!");   //Print message             
+
+            Console.WriteLine("Enter the First Name"); //Take user input
+            String firstName = Console.ReadLine();     //Store input string veriable
+            if (NameValidation(firstName))           //Check firstname NameValidation method
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+
+            Console.WriteLine("\nEnter the Last Name"); // Take user input
+            String lastName = Console.ReadLine();             //Store input string veriable
+
+            if (NameValidation(lastName))             //Check lastName NameValidation method
+            {
+                Console.WriteLine("Valid Last Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Last Name");
+            }
+
+        }
+    }
+
+}
