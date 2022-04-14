8642731666f19c9b8f107ae51ea0e2b60fca8767
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-14 13:36:00 +0530

    [Manoj]Adding_EnterLastName

--- UserRegister.cs
+++ UserRegister.cs
@@ -10,11 +10,20 @@ namespace UserRegistrationProblem
     internal class UserRegister
     {
         // Static Variable
-        public static string firstNamePattern = "^[A-Z]{1}[a-z]{1,}$";
+        public static string firstNamePattern = "^[A-Z]{1}[a-z]{1,}";
+        public static string lastNamePattern = "^[A-Z]{1}[a-z]{1,}";
         //Method to Validate First Name
         public static string ValidateFirstName(string name)
         {
             if (Regex.IsMatch(name, firstNamePattern))
+                return "Valid";
+            else
+                return "InValid";
+        }
+        //Method to Validate Last Name
+        public static string ValidateLastName(string name)
+        {
+            if (Regex.IsMatch(name, lastNamePattern))
                 return "Valid";
             else
                 return "InValid";
