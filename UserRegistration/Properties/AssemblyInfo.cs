ffc1ec4dfda3c5a2c34d22abe9777156b1d9e77c
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-15 01:03:57 +0530

    [Manoj]Adding_FirstName

---
+++ UserRegistration/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UserRegistration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UserRegistration")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e10fe0c7-3c38-4fa3-99a6-1f14b807130f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UserRegistration/Tests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UserRegistrationProblem;
+
+namespace UserRegistration
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestClass]
+        public class UnitTest1
+        {
+            [TestMethod]
+            public void FirstName()
+            {
+                string msg = " ";
+                string expected = "Valid";
+
+                UserRegister p = new UserRegister(msg);
+
+                string actual = p.FirstName("Valid");
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void LastName()
+            {
+                string msg = " ";
+                string expected = "Valid";
+
+                UserRegister p = new UserRegister(msg);
+
+                string actual = p.LastName("Valid");
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void Email()
+            {
+                string msg = " ";
+                string expected = "Invalid";
+
+                UserRegister p = new UserRegister(msg);
+
+                string actual = p.Email("Invalid");
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void MobileNumber()
+            {
+                string msg = " ";
+                string expected = "Invalid";
+
+                    UserRegister p = new UserRegister(msg);
+
+                string actual = p.MobileNumber("Invalid");
+
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void Password()
+            {
+                string msg = " ";
+                string expected = "Invalid";
+
+                UserRegister p = new UserRegister(msg);
+
+                string actual = p.Password("Invalid");
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
