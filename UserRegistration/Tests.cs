be0432ca5c47380df7efdbf1f4342cf19c387fab
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-15 09:44:36 +0530

    [Manoj]Adding_FirstName

---
+++ UserRegistration/Tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UserRegistrationProblem;
+
+namespace UserRegistration
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestClass]
+        public class UnitTest1
+        {
+            [TestMethod]
+            public void FirstName()
+            {
+                string msg = "Manoj ";
+                string expected = "Valid";
+                //Arrange
+                UserRegister p = new UserRegister(msg);
+                //Act
+                string actual = p.FirstName("Valid");
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+           
+
+            [TestMethod]
+            public void LastName()
+            {
+                string msg = "Saiias";
+                string expected = "Valid";
+                //Arrange
+                UserRegister p = new UserRegister(msg);
+                //Act
+                string actual = p.LastName("Valid");
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+
+
+
+
+        }
+
+    }
+}
